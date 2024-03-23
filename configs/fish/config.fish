2aa90136c64e0cfe5d4335975ef53d07da0de400
lighttigerxiv <lighttigerxiv@gmail.com>
2024-03-23 15:14:52 +0000

    add fish config

---
+++ configs/fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#Disable Fish Greeting
+set -U fish_greeting
+
+#Run Starship Prompt
+starship init fish | source
+
+# Environment Variables
+set -Ux CARGO_TARGET_DIR "/home/lighttigerxiv/Coding/Rust/Target"
+
+# Path
+set -gx BUN_INSTALL "$HOME/.bun"
+set PATH $BUN_INSTALL/bin $PATH
+
+# Alias
+alias paru="paru --bottomup"
+alias update="paru -Syyyu; flatpak update"
+alias vencord-installer='sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"'
