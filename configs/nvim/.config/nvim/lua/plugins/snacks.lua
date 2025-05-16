7090510f4f6cc7f42f0fa92e8b8b985ec4de04a4
lighttigerxiv <lighttigerxiv@gmail.com>
2025-05-16 15:27:54 +0100

    feature: add terminal in neovim

--- configs/nvim/.config/nvim/lua/plugins/snacks.lua
+++ configs/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,13 +2,23 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
-	opts = {
-		bigfile = { enabled = true },
-		image = { enabled = true },
-		animate = { enabled = true },
-		input = { enabled = true },
-		notifier = { enabled = true },
-		notify = { enabled = true },
-		rename = { enabled = true },
-	},
+	config = function()
+		require("snacks").setup({
+			{
+				bigfile = { enabled = true },
+				image = { enabled = true },
+				animate = { enabled = true },
+				input = { enabled = true },
+				notifier = { enabled = true },
+				notify = { enabled = true },
+				rename = { enabled = true },
+				terminal = { enabled = true },
+			},
+		})
+
+		local terminal = require("snacks.terminal")
+
+		vim.keymap.set("n", "<leader>t", terminal.toggle, { desc = "Open Terminal" })
+		vim.keymap.set("t", "<esc>", "<C-\\><C-n>", { desc = "Open Terminal" })
+	end,
 }
