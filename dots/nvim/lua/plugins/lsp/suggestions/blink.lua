df79def9cbf6e222c1ad7b167d742630454a651c
lighttigerxiv <lighttigerxiv@gmail.com>
2026-03-30 10:32:01 +0100

    make variables show first on neovim

--- dots/nvim/lua/plugins/lsp/suggestions/blink.lua
+++ dots/nvim/lua/plugins/lsp/suggestions/blink.lua
@@ -14,26 +14,35 @@ return {
 		keymap = {
 			preset = "enter",
 		},
-
 		appearance = {
 			nerd_font_variant = "mono",
 		},
-
 		completion = {
 			documentation = { auto_show = true, auto_show_delay_ms = 500 },
 		},
-
 		sources = {
 			default = { "lsp", "path", "snippets", "lazydev" },
 			providers = {
 				lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
 			},
 		},
-
 		snippets = { preset = "luasnip" },
-
-		fuzzy = { implementation = "lua" },
-
+		fuzzy = {
+			implementation = "lua",
+			sorts = {
+				function(a, b)
+					local snippet = require("blink.cmp.types").CompletionItemKind.Snippet
+					if a.kind == snippet and b.kind ~= snippet then
+						return false
+					end
+					if a.kind ~= snippet and b.kind == snippet then
+						return true
+					end
+				end,
+				"score",
+				"sort_text",
+			},
+		},
 		signature = { enabled = true },
 	},
 }
