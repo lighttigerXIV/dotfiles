2b6f270cf39a40b347cb4a887d360d549f397adc
lighttigerxiv <lighttigerxiv@gmail.com>
2026-04-10 10:01:50 +0100

    update: make toggleterm also work in PT layout

--- dots/nvim/lua/plugins/ui/panels/toggleterm.lua
+++ dots/nvim/lua/plugins/ui/panels/toggleterm.lua
@@ -4,11 +4,14 @@ return {
 		local terminal = require("toggleterm")
 
 		terminal.setup({
-			open_mapping = [[<c-`>]],
+			open_mapping = nil,
 			direction = "float",
 			float_opts = {
 				border = "curved",
 			},
 		})
+
+		vim.keymap.set({ "n", "t", "i" }, "<c-`>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal (US Layout)" })
+		vim.keymap.set({ "n", "t", "i" }, "<c-\\>", "<cmd>ToggleTerm<cr>", { desc = "Toggle terminal (PT Layout)" })
 	end,
 }
